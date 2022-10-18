ceff3ea445dada855fa3b22138ed3a7ab347ff31
Nursyahna Puteri <114853207+NurSyahnaPuterii@users.noreply.github.com>
2022-10-18 19:29:41 +0800

    first commit

---
+++ PRAK202-2210817120007-NursyahnaPuteri.py
@@ -0,0 +1,5 @@
+nilaipertama = float(input ("masukan nilai pertama : "))
+nilaikedua = float(input ("masukan nilai kedua : "))
+
+hasil = nilaipertama + nilaikedua
+print ("Hasil dari penjumlahan nilai pertama %.2f dan nilai kedua %.2fadalah %.2f"%(nilaipertama, nilaikedua, hasil,))
