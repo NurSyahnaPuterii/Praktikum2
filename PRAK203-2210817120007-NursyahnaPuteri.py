ec3bddfd08ddf53afe38e46e1df2080898499632
Nursyahna Puteri <114853207+NurSyahnaPuterii@users.noreply.github.com>
2022-10-18 19:31:16 +0800

    first commit

---
+++ PRAK203-2210817120007-NursyahnaPuteri.py
@@ -0,0 +1,10 @@
+
+a = float (input ("masukan nilai pertama : "))
+b = float (input ("masukan nilai kedua : "))
+i = float (input ("masukan nilai ketiga : "))
+j = float (input ("masukan nilai keempat : "))
+x = float (input ("masukan nilai kelima : "))
+y = float (input ("masukan nilai keenam : "))
+
+hasil = (a - b) * (i / j) - (x + y )
+print ("Hasil dari perhitungan tersebut adalah {:.3f}".format (hasil))
